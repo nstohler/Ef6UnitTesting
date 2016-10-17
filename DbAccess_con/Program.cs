97876db1090174b38c50126f96235add8559a6e5
nstohler <nstohler@gmail.com>
2016-10-17 17:49:31 +0200

    added console and test projects

---
+++ DbAccess_con/Program.cs
@@ -0,0 +1,25 @@
+﻿using DbAccessLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccess_con
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var name = "nicolas";
+			Console.WriteLine($"hello {name}");
+
+			using (var context = new PersonDbContext())
+			{
+				var dbAccess = new PersonDbService(context);
+				var personSet = dbAccess.GetAllPersons();
+
+			}
+		}
+	}
+}
