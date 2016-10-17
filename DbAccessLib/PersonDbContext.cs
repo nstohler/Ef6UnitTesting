d8eb186e0e9fda7f0215f8b91c864f65224916c5
nstohler <nstohler@gmail.com>
2016-10-17 17:24:52 +0200

    Init

---
+++ DbAccessLib/PersonDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccessLib
+{
+	public class PersonDbContext : DbContext
+	{
+		public virtual DbSet<Person> PersonSet { get; set; }
+	}
+}
---
+++ DbAccessLib/PersonDbService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccessLib
+{
+	public class PersonDbService
+	{
+		private readonly PersonDbContext _personDbContext;
+
+		public PersonDbService(PersonDbContext personDbContext)
+		{
+			_personDbContext = personDbContext;
+		}
+
+		public Person AddPerson(Person p)
+		{
+			var r = _personDbContext.PersonSet.Add(p);
+			_personDbContext.SaveChanges();
+
+			return r;
+		}
+
+		public List<Person> GetAllPersons()
+		{
+			var query = _personDbContext.PersonSet.AsNoTracking()
+						.OrderBy(x => x.LastName);
+
+			return query.ToList();
+		}
+	}
+}
